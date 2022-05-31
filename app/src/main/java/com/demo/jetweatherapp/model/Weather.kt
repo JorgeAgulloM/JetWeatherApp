51c5db0b22150036b535a672d1df71bb15268aad
Jorge Agullo <agullojorge@gmail.com>
2022-05-31 08:27:48 +0200

    [FEATURE] WIP - Added network & refactor files

--- app/src/main/java/com/demo/jetweatherapp/model/Weather.kt
+++ app/src/main/java/com/demo/jetweatherapp/model/Weather.kt
@@ -1,1 +1,1 @@
-package com.demo.jetweatherapp.modeldata class Weather(    val city: City,    val cnt: Int,    val cod: String,    val list: List<WeatherObject>,    val message: Double)
+package com.demo.jetweatherapp.modeldata class Weather(    val city: City,    val cnt: Int,    val cod: String,    val list: List<WeatherItem>,    val message: Double)
--- app/src/main/java/com/demo/jetweatherapp/model/WeatherX.kt
+++
@@ -1,1 +0,0 @@
-package com.demo.jetweatherapp.modeldata class WeatherX(    val description: String,    val icon: String,    val id: Int,    val main: String)
