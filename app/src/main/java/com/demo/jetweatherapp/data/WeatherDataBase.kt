5961d896d1450584ee89d9ba8b3ed9c05fe40737
Jorge Agullo <agullojorge@gmail.com>
2022-06-01 14:56:09 +0200

    [FEATURE] WIP created WeatherDataBase.kt

---
+++ app/src/main/java/com/demo/jetweatherapp/data/WeatherDataBase.kt
@@ -0,0 +1,1 @@
+package com.demo.jetweatherapp.dataimport androidx.room.Databaseimport androidx.room.RoomDatabaseimport com.demo.jetweatherapp.model.Favorite@Database(entities = [Favorite::class], version = 1, exportSchema = false)abstract class WeatherDataBase: RoomDatabase() {    abstract fun weatherDao(): WeatherDao}
